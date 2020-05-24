10a1240fb6add63da014c3478024a2926cf92f29
olan <olan@olan.com>
2020-05-24 21:58:23 +0100

    fixed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
 
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2004);
+serv.listen(2005);
 console.log('*** STARTING SERVER ***');
 
 var playerCount = 1;
